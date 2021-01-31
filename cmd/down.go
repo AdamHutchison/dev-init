f3200ce67b6aa2a28ad0eff71449b74f2d12b4c6
Adam Hutchison <adam@manjaro.home>
2021-01-31 08:49:22 +0000

    Adds down cmd

--- cmd/down.go
+++ cmd/down.go
@@ -1,65 +1,35 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"fmt"
-	"os/exec"
 	"github.com/spf13/cobra"
+	"github.com/AdamHutchison/dev-init/utils"
+	"os/exec"
 	"strings"
 )
 
 // downCmd represents the down command
 var downCmd = &cobra.Command{
 	Use:   "down",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Kill all running containsers",
 	Run: func(cmd *cobra.Command, args []string) {
-		out, err := exec.Command("docker", "ps", "-q").Output()
-		containers := strings.TrimSuffix(string(out), "\n")
-		fmt.Println(string(containers))
-		command := exec.Command("docker", "stop", string(containers))
-
-		stdout, err := command.StdoutPipe()
-
-		command.Stderr = command.Stdout
+		out, err:= exec.Command("docker", "ps", "-q").Output()
 
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		if err = command.Start(); err != nil {
-			fmt.Println(err)
-			return
+		split := strings.Split(string(out), "\n")
+
+		if len(split) > 0 {
+    		split = split[:len(split)-1]
 		}
 
-		for {
-			tmp := make([]byte, 1024)
-			_, err := stdout.Read(tmp)
-			fmt.Print(string(tmp))
-			if err != nil {
-				break
-			}
-		}
+		arguments := append([]string{"stop"}, split...)
+
+		fmt.Println("Stopping the following containers:", split)
+		utils.Exec("docker", arguments...)
 	},
 }
 
--- cmd/up.go
+++ cmd/up.go
@@ -1,24 +1,9 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"fmt"
-	"os/exec"
 	"github.com/spf13/cobra"
+	"github.com/AdamHutchison/dev-init/utils"
 )
 
 // upCmd represents the up command
@@ -26,30 +11,12 @@ var upCmd = &cobra.Command{
 	Use:   "up",
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
-		command := exec.Command("docker-compose", "--file", "./docker-local/docker-compose.yml", "up", "-d")
+		command := "docker-compose"
+		arguments := []string{"--file", "./docker-local/docker-compose.yml", "up", "-d"}
 
-		stdout, err := command.StdoutPipe()
+		fmt.Println("Running docker-compose --file ./docker-local/docker-compose.yml up -d")
 
-		command.Stderr = command.Stdout
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if err = command.Start(); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		for {
-			tmp := make([]byte, 1024)
-			_, err := stdout.Read(tmp)
-			fmt.Print(string(tmp))
-			if err != nil {
-				break
-			}
-		}
+		utils.Exec(command, arguments...)
 	},
 }
 
