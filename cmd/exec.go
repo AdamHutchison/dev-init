b3ad0c69f6d612a7e03371b14f1240edb87d59ef
Adam Hutchison <adam@manjaro.home>
2021-02-02 23:08:01 +0000

    Updates test command

--- cmd/exec.go
+++ cmd/exec.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"github.com/AdamHutchison/dev-init/utils"
+	"strings"
 )
 
 // execCmd represents the exec command
@@ -25,22 +26,14 @@ var execCmd = &cobra.Command{
 	Use:   "exec",
 	Short: "Exec a command with the php container",
 	Run: func(cmd *cobra.Command, args []string) {
+		command := "export XDEBUG_MODE=develop && " + strings.Join(args, " ")
 
-		arguments := append([]string{"--file", "docker-local/docker-compose.yml", "exec", "-T", "php"}, args...)
+		arguments := []string{"--file", "docker-local/docker-compose.yml", "exec", "-T", "php" ,"/bin/bash", "-c", command}
+
 		utils.Exec("docker-compose", arguments...)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(execCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// execCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// execCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/test.go
+++ cmd/test.go
@@ -1,23 +1,7 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"fmt"
 	"github.com/AdamHutchison/dev-init/utils"
 )
 
@@ -26,22 +10,17 @@ var testCmd = &cobra.Command{
 	Use:   "test",
 	Short: "Exec a command with the php container",
 	Run: func(cmd *cobra.Command, args []string) {
-
 		command := "./vendor/bin/phpunit"
 
-		arguments := []string{"--file", "docker-local/docker-compose.yml", "exec", "-T", "php", "/bin/bash", "-c"}
+		debug, _ := cmd.Flags().GetBool("debug")
+		filter, _ := cmd.Flags().GetString("filter")
+		stop, _ := cmd.Flags().GetBool("stop-on-failure")
 
-		debug, err := cmd.Flags().GetBool("debug")
+		addFilter(&command, filter)
+		addDebug(&command, debug)
+		addStopOnFailure(&command, stop)
 
-		if err != nil {
-			fmt.Println(utils.Fatal(err))
-		}
-
-		if debug != true {
-			command = "export XDEBUG_MODE=develop && " + command
-		}
-		
-		arguments = append(arguments, command)
+		arguments := append(getBaseCommand(), command)
 
 		utils.Exec("docker-compose", arguments...)
 	},
@@ -50,5 +29,28 @@ var testCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(testCmd)
 	testCmd.Flags().BoolP("debug", "d", false, "Enable Xdebug")
+	testCmd.Flags().BoolP("stop-on-failure", "s", false, "Stop test run on first failure")
+	testCmd.Flags().String("filter", "", "Test class to filter by")
+}
 
+func getBaseCommand () []string {
+	return []string{"--file", "docker-local/docker-compose.yml", "exec", "-T", "php", "/bin/bash", "-c"}
+}
+
+func addFilter(command *string, filter string) {
+	if filter != "" {
+		*command = *command + " --filter " + filter
+	}
+}
+
+func addDebug(command *string, debug bool) {
+	if debug != true {
+		*command = "export XDEBUG_MODE=develop && " + *command
+	}
+}
+
+func addStopOnFailure(command *string, stop bool) {
+	if stop != false {
+		*command = *command + " --stop-on-failure"
+	}
 }
