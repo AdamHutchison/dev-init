fc9e926c439dbdb233a64ae6bb9482559cef380c
Adam Hutchison <adam@manjaro.home>
2021-02-04 21:55:23 +0000

    Adds cron and queue containers

---
+++ utils/containers.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"strings"
+	"fmt"
+	"os/exec"
+)
+
+func GetRunningContainers() []string {
+	out, err := exec.Command("docker", "ps", "-q").Output()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	split := strings.Split(string(out), "\n")
+
+	if len(split) > 0 {
+		split = split[:len(split)-1]
+	}
+
+	return split
+}
